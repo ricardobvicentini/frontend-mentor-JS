7501211e43b1ef75a2f601dadeba399fa9a385a9
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-03-26 18:13:47 -0300

    Update script.js

--- age-calculator-app/script.js
+++ age-calculator-app/script.js
@@ -34,12 +34,28 @@ function ageCalc() {
   daysResult.innerHTML = `<em>${day}</em>`;
 }
 
-function checkValidDate() {
-  /* const dateInput = `${year1.value}, ${month1.value}, ${day1.value}`; */
-  const dateInput = `2001/06/31`;
-  const testDate = new Date(dateInput);
-  console.log(testDate);
+/* Existing date */
+
+function daysInMonth(m, y) {
+  switch (m) {
+    case 1:
+      return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
+    case 8:
+    case 3:
+    case 5:
+    case 10:
+      return 30;
+    default:
+      return 31;
+  }
 }
+
+isValidDate = function (d, m, y) {
+  m = parseInt(m, 10) - 1;
+  return m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y);
+};
+
+/* --------------- */
 
 function invalidDate() {
   if (day1.value > 31) {
